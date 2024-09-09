4a862a7a7361587b27c2109ed0f68012ebcba983
Filip Herbst <filip_herbst@yahoo.com>
2024-09-08 23:38:12 -0500

    Palindrome Exercise Completed

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalindromeExercise;
+
+
+//Note to self, source: https://www.designgurus.io/answers/detail/how-to-reverse-a-string-in-c
+public class WordSmith
+{
+    public bool IsAPalindrome(string input)
+    {
+        if (input == null)
+        {
+            return false;
+        }
+
+        char[] inputAsCharArray = input.ToCharArray();          //create new char array, convert string input to char array
+        // char[] inputAsCharArray = new char[] { };
+        // inputAsCharArray = input.ToCharArray();
+        Array.Reverse(inputAsCharArray);                        //Array Class, Reverse Method to char array, argument inputAsCharArray
+        string reversedString = new string(inputAsCharArray);   //declare new string from reversed char array
+
+        if (input.ToLower() == reversedString.ToLower())        //If statement, if original input and reversed input are equal, input is a palindrome
+        {                                                       //Use ToLower for original and reversed to account for capitalization
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        // List<char> stringAsListForward = new List<char>{};
+        // List<char> stringAsListBackward = new List<char>{};
+        //
+        // var inputOriginal = input.ToList();
+        // stringAsListForward.Add(inputOriginal);
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("hello", false)]
+        [InlineData("1483", false)]
+        [InlineData("yay", true)]
+        [InlineData("Yay", true)]
+        [InlineData("12321", true)]
+        [InlineData(null, false)]
+        
+        public void PalindromeTest(string input, bool expected)
         {
+            //Arrange:
+            var tryWord = new WordSmith();
 
+            //Act:
+            var actual = tryWord.IsAPalindrome(input);
+
+            //Assert:
+            Assert.Equal(expected, actual);
         }
     }
 }
